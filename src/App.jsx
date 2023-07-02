439bde28a6cc12a39755f189650c6ef7453f7fe5
shobhit-28 <shobhitraj34@gmail.com>
2023-07-02 23:33:12 +0530

    added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom"
 import { Navbar } from "./components/navBar/navbar"
+import { HomePage } from "./pages/homePage/HomePage"
 
 function App() {
 
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   )
 }
