172fcfa4c95d2d5b1c5f0436c1ed1e53d509f181
shobhit-28 <shobhitraj34@gmail.com>
2023-07-02 23:21:17 +0530

    made navbar ui responsive

--- src/components/navBar/navbar.jsx
+++ src/components/navBar/navbar.jsx
@@ -1,21 +1,66 @@
+import { useEffect, useRef, useState } from 'react'
 import './navbar.css'
 
+import { BsFilterRight } from 'react-icons/bs'
+import { AiOutlineClose } from 'react-icons/ai'
+
 export const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const menuRef = useRef(null)
+    const navbarRef = useRef(null)
+
+    useEffect(() => {
+        const handleOutsideClick = (e) => {
+            if (!menuRef?.current?.contains(e?.target)) {
+                setIsMenuOpen(false)
+            }
+        }
+        document.addEventListener("mousedown", handleOutsideClick, true)
+        document.addEventListener("scroll", () => setIsMenuOpen(false))
+        return () => {
+            document.removeEventListener("mousedown", handleOutsideClick)
+            document.removeEventListener("scroll", () => setIsMenuOpen(false))
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+
     return (
-        <div className="nav-bar">
+        <div className="nav-bar" ref={navbarRef}>
             <div className="left-section">
-            <div className="header-img-container">
-                <img src= "src\Resources\MyImages\DSC_0869~3.JPG" alt="" />
-            </div>
-            <p className="header">
-                Shobhit Raj
-            </p>
+                <div className="header-img-container">
+                    <img src="src\Resources\MyImages\DSC_0869~3.JPG" alt="" />
+                </div>
+                <p className="header">
+                    Shobhit Raj
+                </p>
             </div>
             <div className="right-section">
-                <ul className="nav-links">
+                <ul className="nav-links lg-links">
                     <li className="nav-link">Projects</li>
                     <li className="nav-link">Resume</li>
                 </ul>
+                <div className="menu-container">
+                    {isMenuOpen
+                        ?
+                        <button className="icon" onClick={() => isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true)}>
+                            <AiOutlineClose />
+                        </button>
+                        :
+                        <button className="icon" onClick={() => setIsMenuOpen(true)}>
+                            <BsFilterRight />
+                        </button>
+                    }
+                    {isMenuOpen &&
+                        <div className="menu" ref={menuRef}>
+                            <ul className="nav-links">
+                                <li className="nav-link">Projects</li>
+                                <li className="nav-link">Resume</li>
+                            </ul>
+                        </div>
+                    }
+                </div>
             </div>
         </div>
     )
