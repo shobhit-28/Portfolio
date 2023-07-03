5bc32308500821d2eb4e2ecce6fe2e65acbc098d
shobhit-28 <shobhitraj34@gmail.com>
2023-07-03 23:14:31 +0530

    homepage done

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,19 +1,51 @@
 import './HomePage.css'
 
+import { AiFillHtml5 } from 'react-icons/ai'
+import { SiCss3, SiJest, SiRedux, SiTailwindcss, SiTypescript } from 'react-icons/si'
+import { IoLogoJavascript, IoLogoPython } from 'react-icons/io'
+import { FaReact } from 'react-icons/fa'
+import { FaArrowRightLong } from 'react-icons/fa6'
+
 export const HomePage = () => {
 
     return (
         <div className='home-page page'>
             <div className="top-content">
                 <div className="contents">
-                    <p className="head">Hello, I am <br />Shobhit Raj.</p>
+                    <p className="head">Shobhit Raj <br /> <span className="info">Frontend Web Developer</span></p>
                 </div>
                 <div className="img-container">
                     <img src='src\Resources\MyImages\DSC_0989~2.JPG' alt="" title='Shobhit Raj' />
                 </div>
             </div>
             <section id="skills" className="skills">
-                <p>Skills</p>
+                <div className="gif-container" style={{ width: '18rem' }}>
+                    <img src="src\Resources\gifs\1_partyanimsm2.gif" alt="" />
+                </div>
+                <div className="skill-sec">
+                    <p className='skill-heading'>My Skills</p>
+                    <ul className="skills-list">
+                        <li className="skill">
+                            <span className="skill-head">Technical Skills<FaArrowRightLong className='arrow-right' /></span>
+                            <span className="skill-icons">
+                                <AiFillHtml5 title='HTML' />
+                                <SiCss3 title='CSS' />
+                                <IoLogoJavascript title='Javascript' />
+                                <SiTypescript title='Typescript' />
+                                <IoLogoPython title='Python' />
+                            </span>
+                        </li>
+                        <li className="skill">
+                            <span className="skill-head">Libraries/Frameworks<FaArrowRightLong className='arrow-right' /></span>
+                            <span className="skill-icons">
+                                <FaReact title='React' />
+                                <SiJest title='Jest' />
+                                <SiTailwindcss title='Tailwind' />
+                                <SiRedux title='Redux' />
+                            </span>
+                        </li>
+                    </ul>
+                </div>
             </section>
         </div>
     )
