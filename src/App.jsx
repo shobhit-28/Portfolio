b86656748436a8d67f3c293668af29bc601204f1
shobhit-28 <shobhitraj34@gmail.com>
2023-07-04 00:57:23 +0530

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import { Navbar } from "./components/navBar/navbar"
 import { HomePage } from "./pages/homePage/HomePage"
+import { Footer } from "./components/footer/footer"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-no-target-blank */
+import './footer.css'
+
+import {AiFillGithub, AiFillLinkedin, AiOutlineTwitter} from 'react-icons/ai'
+
+export const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="name-img">
+            <div className="img-container">
+                <img src="src\Resources\MyImages\DSC_0869~3.JPG" alt="" />
+            </div>
+            <p className="name">Shobhit Raj</p>
+        </div>
+        <div className="socials">
+            <a className="linkedin" href='https://www.linkedin.com/in/shobhit-raj-19a588215/' target='_blank'><AiFillLinkedin /></a>
+            <a className="github" href='https://github.com/shobhit-28/' target='_blank'><AiFillGithub /></a>
+            <a className="twitter" href='https://twitter.com/Shobhit28573001' target='_blank'><AiOutlineTwitter /></a>
+        </div>
+        <p className="note">© No Copyright, feel free to copy</p>
+    </div>
+  )
+}
