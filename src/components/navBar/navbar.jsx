139eefd108e2cf7cfa4bccd2d5b37134631302af
shobhit-28 <shobhitraj34@gmail.com>
2025-05-29 22:13:04 +0530

    resume updated

--- src/components/navBar/navbar.jsx
+++ src/components/navBar/navbar.jsx
@@ -49,7 +49,7 @@ export const Navbar = () => {
                 <ul className="nav-links lg-links">
                     <li className="nav-link" onClick={() => projectClickHandler()}>Projects</li>
                     <li className="nav-link">
-                        <a href="https://drive.google.com/file/d/1_OSC9hz9AVRGGknWB-_we5GJYoIQAQWD/view?usp=sharing" className="link" target='_blank' rel="noreferrer">
+                        <a href="https://drive.google.com/file/d/1qf-vmvT5X7WM9xz_-6QB9DM9838KRYtu/view" className="link" target='_blank' rel="noreferrer">
                             Resume
                         </a>
                     </li>
